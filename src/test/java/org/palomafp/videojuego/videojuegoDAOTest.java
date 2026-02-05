f13cdd77156799acf779af1eba95f7fd933b63d5
AlejandroJ-GS <aljogosom@gmail.com>
2026-02-05 21:23:41 +0100

    test realizado

--- src/test/java/org/palomafp/videojuego/videojuegoDAOTest.java
+++ src/test/java/org/palomafp/videojuego/videojuegoDAOTest.java
@@ -11,22 +11,24 @@ import org.junit.jupiter.api.Test;
 public class videojuegoDAOTest {
     @Test
     public void testVideojuegoDAO() {
-        private Mapa[] mapas;
-    private Usuario[] usuarios;
-    private Personaje[] personajes;
-       VideojuegoDAO dao = new VideojuegoDAO();
+        Mapa[] mapas = new Mapa[4];
+        Zona[] zonas = new Zona[2];
+        Videojuego[] videojuegos = new Videojuego[7];
+        Usuario[] usuarios1=new Usuario[6];
+        Personaje[] personajes1=new Personaje[7];
+        VideojuegoDAO dao = new VideojuegoDAO();
         Zona z1 = new Zona("Grande", "Una zona amplia con muchos enemigos", "Bosque");
-        Mapa m1 = new Mapa("Mediano", "Un mapa con varios caminos y obstáculos", 2, 5, zonaLista);
+        Mapa m1 = new Mapa("Mediano", "Un mapa con varios caminos y obstáculos", 2, 5, zonas);
         Personaje p1 = new Personaje("Guerrero", "Un personaje fuerte y resistente", 10);
-        Videojuego v1 = new Videojuego("Aventura", "PC", 59.99, mapaLista, usuarioos, personajes );
-        Usuario u1 = new Usuario("Jugador1", "10", "Nivel 5", videojuegos[]);
-       String resultado="VideojuegoDAO{" +
+        Videojuego v1 = new Videojuego("Aventura", "PC", 59.99, mapas, usuarios1, personajes1);
+        Usuario u1 = new Usuario("Jugador1", "10", "Nivel 5", videojuegos);
+    
+       assertEquals(dao.VideojuegoInfo(),"VideojuegoDAO{" +
                 "zonaLista=" + z1 +
                 ", mapaLista=" + m1 +
                 ", usuarioos=" + u1 +
                 ", personajes=" + p1 +
                 ", videojuegos=" + v1 +
-                '}';
-       assertEquals(resultado,dao.VideojuegoInfo(),resultado );
+                '}');
     }
 }
