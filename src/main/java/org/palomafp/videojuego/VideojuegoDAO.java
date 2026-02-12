129d8464f64eb1ef30c204819a5979336cb26e4c
AlejandroJ-GS <aljogosom@gmail.com>
2026-02-12 09:42:08 +0100

    cambio dao

--- src/main/java/org/palomafp/videojuego/VideojuegoDAO.java
+++ src/main/java/org/palomafp/videojuego/VideojuegoDAO.java
@@ -11,22 +11,21 @@ import org.palomafp.videojuego.modelo.Zona;
 public class VideojuegoDAO {
     public Videojuego getVideojuegoRandom() {
         ArrayList <Zona> zonaLista=new ArrayList <Zona> ();
-        Mapa[] mapaLista=new Mapa[4];
-        Usuario[] usuarioos=new Usuario[6];
-        Personaje[] personajes=new Personaje[7];
-        Videojuego[] videojuegos=new Videojuego[7];
+        ArrayList <Mapa> mapaLista=new ArrayList <Mapa>();
+        ArrayList <Usuario> usuarioos=new ArrayList <Usuario>();
+        ArrayList <Personaje> personajes=new ArrayList <Personaje>();
+       ArrayList <Videojuego> videojuegos=new ArrayList <Videojuego>();
 
         Zona z1 = new Zona("Grande", "Una zona amplia con muchos enemigos", "Bosque");
-
+        zonaLista.add(z1);
         Mapa m1 = new Mapa("Mediano", "Un mapa con varios caminos y obstáculos", 2, 5, zonaLista);
-
+        mapaLista.add(m1);
         Personaje p1 = new Personaje("Guerrero", "Un personaje fuerte y resistente", 10);
-        personajes[0] = p1;
-        
-        Videojuego v1 = new Videojuego("Aventura", "PC", 59.99, mapaLista, usuarioos, personajes );
-
+        personajes.add(p1) ;
+        Videojuego v1 = new Videojuego("Aventura", "PC", 59.99, mapaLista, usuarioos, personajes , "1"   );
+        videojuegos.add(v1);
         Usuario u1 = new Usuario("Jugador1", "10", "Nivel 5", videojuegos);
-        System.out.println(u1);
+        usuarioos.add(u1);
 
         return v1;
     }
