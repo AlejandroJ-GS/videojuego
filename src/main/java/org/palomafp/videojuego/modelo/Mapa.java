7be51d4cc646c9d724774a924975e7b54f227bf1
AlejandroJ-GS <aljogosom@gmail.com>
2026-03-11 14:28:12 +0100

    comentarios añadidos

--- src/main/java/org/palomafp/videojuego/modelo/Mapa.java
+++ src/main/java/org/palomafp/videojuego/modelo/Mapa.java
@@ -1,14 +1,25 @@
 package org.palomafp.videojuego.modelo;
 
 import java.util.ArrayList;
-
+/**
+ * es un objeto de tipo mapa que tiene una serie de atributos (tamaño , descripcion , zonas numUsuario y un arrays de las zonas )
+ * @author Alejandro y Cristina
+ * @version 1.0
+ */
 public class Mapa {
     private String tamaño;
     private String descripcion;
     private int zonas;
     private int numUsuario;
     private ArrayList<Zona> zonasLista=null;
-
+/**
+ * constructor de mapa donde definimos los atributos de mapa  
+ * @param tamaño se trata del tamaño del mapa en metros
+ * @param descripcion se trata de una explicacion tecnica del mapa
+ * @param zonas se trata del numero de zonas
+ * @param numUsuario se trata del numero de personas en el mapa
+ * @param zonasLista se trata una lista de todas las zonas
+ */
     public Mapa(String tamaño, String descripcion, int zonas, int numUsuario, ArrayList<Zona> zonasLista) {
         this.tamaño = tamaño;
         this.descripcion = descripcion;
@@ -16,47 +27,79 @@ public class Mapa {
         this.numUsuario = numUsuario;
         this.zonasLista = zonasLista;
     }
-
+/**
+ * nos devuelve el tamaño del mapa
+ * @return 
+ */
     public String getTamaño() {
         return tamaño;
     }
-
+/**
+ * establecemos el tamaño del mapa
+ * @param tamaño
+ */
     public void setTamaño(String tamaño) {
         this.tamaño = tamaño;
     }
-
+/**
+ * nos devuelve la descripcion del mapa
+ * @return 
+ */
     public String getDescripcion() {
         return descripcion;
     }
-
+/**
+ * establecemos la descripcion del mapa
+ * @param descripcion
+ */
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
-
+/**
+ * nos devuelve las zonas del mapa
+ * @return 
+ */
     public int getZonas() {
         return zonas;
     }
-
+/**
+ * establecemos las zonas del mapa
+ * @param zonas 
+ */
     public void setZonas(int zonas) {
         this.zonas = zonas;
     }
-
+/**
+ * nos devuelve el numero de usuarios del mapa
+ * @return 
+ */
     public int getNumUsuario() {
         return numUsuario;
     }
-
+/**
+ * establecemos el numero de usuarios del mapa
+ * @param numUsuario
+ */
     public void setNumUsuario(int numUsuario) {
         this.numUsuario = numUsuario;
     }
-
+/**
+ * nos devuelve una lista de las zonas del mapa
+ * @return 
+ */
     public ArrayList<Zona> getZonasLista() {
         return zonasLista;
     }
-
+/**
+ * establecemos una lista de las zonas del mapa del mapa
+ * @param tamaño
+ */
     public void setZonasLista(ArrayList<Zona> zonasLista) {
         this.zonasLista = zonasLista;
     }
-
+/**
+ * es un metodo sobre el mapa que nos dice todos los atributos
+ */
     @Override
     public String toString() {
         return "Mapa{" +
--- src/main/java/org/palomafp/videojuego/modelo/Personaje.java
+++ src/main/java/org/palomafp/videojuego/modelo/Personaje.java
@@ -1,39 +1,69 @@
 package org.palomafp.videojuego.modelo;
+/**
+ * es un objeto de tipo personaje que tiene una serie de atributos (rol , descripcion , nivel )
+ * @author Alejandro y Cristina
+ * @version 1.0
+ */
 public class Personaje {
     private String rol;
     private String descripcion;
     private int nivel;
-
+/**
+ * constructor de personaje donde definimos los atributos de personaje 
+ * @param rol se trata del rol del personaje , de que tipo es etc
+ * @param descripcion se trata de la descripcion del personaje
+ * @param nivel se trata del nivel del personaje
+ */
     public Personaje(String rol, String descripcion, int nivel) {
         this.rol = rol;
         this.descripcion = descripcion;
         this.nivel = nivel;
     }
-
+/**
+ * nos devuelve el rol del personaje
+ * @return
+ */
     public String getRol() {
         return rol;
     }
-
+/**
+ * establecemos el rol del personaje
+ * @param rol
+ */
     public void setRol(String rol) {
         this.rol = rol;
     }
-
+/**
+ * nos devuelve la descripcion del personaje
+ * @return
+ */
     public String getDescripcion() {
         return descripcion;
     }
-
+/**
+ * establecemos la descripcion del personaje
+ * @param descripcion
+ */
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
-
+/**
+ * nos devuelve el nivel del personaje
+ * @return
+ */
     public int getNivel() {
         return nivel;
     }
-
+/**
+ * establecemos el nivel del personaje
+ * @param nivel
+ */
     public void setNivel(int nivel) {
         this.nivel = nivel;
     }
-
+/**
+ * es un metodo sobre el personaje que nos dice todos los atributos
+ */
     @Override
     public String toString() {
         return "Personaje{" +
