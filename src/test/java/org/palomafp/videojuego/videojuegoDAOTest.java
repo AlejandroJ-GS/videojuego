3a998d33c8b0a66eb0eb61d55b31f82f37e615d1
AlejandroJ-GS <aljogosom@gmail.com>
2026-03-15 19:16:15 +0100

    añadido comentarios de la clase test

--- src/test/java/org/palomafp/videojuego/videojuegoDAOTest.java
+++ src/test/java/org/palomafp/videojuego/videojuegoDAOTest.java
@@ -7,8 +7,15 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 import org.junit.jupiter.api.Test;
-
+/**
+ * Clase de pruebas para comprobar el funcionamiento de los métodos
+ * de la clase VideojuegoDAO.
+ */
 public class videojuegoDAOTest {
+    /**
+     * Comprueba que el método getVideojuegoRandom devuelve un videojuego
+     * no nulo, con código distinto de 0 y con mapas asociados.
+     */
     @Test
     public void testVideojuegoRandom() {
         VideojuegoDAO videojuegosDAO = new VideojuegoDAO();
@@ -19,7 +26,10 @@ public class videojuegoDAOTest {
         assertNotNull(videojuego1.getMapas());
         
     }
-
+ /**
+     * Comprueba que el método getVideoJuegoByCod devuelve un videojuego
+     * con mapas no nulos y cuyo género es "Aventura".
+     */
     @Test
     public void testVideojuegoDAOByCod() {
         VideojuegoDAO videojuegosDAO= new VideojuegoDAO();
@@ -27,6 +37,11 @@ public class videojuegoDAOTest {
         assertNotNull(videojuego1.getMapas());
         assertEquals(videojuego1.getGenero(), "Aventura");
     }
+
+    /**
+     * Comprueba que la lista de videojuegos no es nula
+     * y que contiene 4 videojuegos.
+     */
     @Test
     public void testVideojuegoDAOGetAll() {
         VideojuegoDAO videojuegosDAO= new VideojuegoDAO();
